df8ae6ee048e54565d62b2e6236c92e4a857104c
Justus Massmann <justusmassmann@online.de>
2022-04-25 10:41:32 +0200

    Define Home Route with React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>List of burger restaurants</h1>
+    </main>
+  );
+}
